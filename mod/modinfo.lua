b2db87e023b6a8d38f49c8ea296b218afb1c94d5
Gérison Sabino <gerison@vbsbrasil.net>
2023-03-09 19:22:36 -0300

    config rework debuffs

--- mod/modinfo.lua
+++ mod/modinfo.lua
@@ -1,8 +1,8 @@
 name = "Classic Wes"
-description = "Set classic Don't Starve stats for Wes:\n\n113 Health\n113 Hunger\n150 Sanity\n1.25x Hunger Modifier\n\nConfigure Mod to define each stat for \"Classic\" or \"Standard\"."
+description = "Set classic Don't Starve stats for Wes and disable some Rework debuffs:\n\n113 Health\n113 Hunger\n150 Sanity\n1.25x Hunger Modifier\n\nDisabled Hounded Target\nDisabled Insulation Modifier (Sets old insulation modifier)\nDisabled Grogginess Modifier\nDisabled Lightning Modifier\n\nConfigure Mod to define each stat for \"Classic\" or \"Standard\".\n"
 author = "Geeris"
 
-version = "1.2.1"
+version = "1.3.0"
 forumthread = ""
 api_version = 10
 
@@ -20,7 +20,7 @@ configuration_options = {
 	{
 		name = "SET_CLASSIC_HEALTH",
 		label = "Health",
-		hover = "Sets the amount of Health.",
+		hover = "Amount of Health.",
 		default = true,
 		options = {
 			{ description = "Classic (113)", data = true },
@@ -30,7 +30,7 @@ configuration_options = {
 	{
 		name = "SET_CLASSIC_HUNGER",
 		label = "Hunger",
-		hover = "Sets the amount of Hunger.",
+		hover = "Amount of Hunger.",
 		default = true,
 		options = {
 			{ description = "Classic (113)", data = true },
@@ -40,7 +40,7 @@ configuration_options = {
 	{
 		name = "SET_CLASSIC_SANITY",
 		label = "Sanity",
-		hover = "Sets the amount of Sanity.",
+		hover = "Amount of Sanity.",
 		default = true,
 		options = {
 			{ description = "Classic (150)", data = true },
@@ -50,11 +50,51 @@ configuration_options = {
 	{
 		name = "SET_CLASSIC_HUNGER_RATE",
 		label = "Hunger Modifier",
-		hover = "Sets the rate of Hunger Modifier.",
+		hover = "Rate of Hunger Modifier.",
 		default = true,
 		options = {
 			{ description = "Classic (1.25x)", data = true },
 			{ description = "Standard (1.00x)", data = false }
+		}
+	},
+	{
+		name = "SET_CLASSIC_HOUNDED_TARGET",
+		label = "Hounded Target",
+		hover = "Hounds waves will focus more on Wes than the other survivors.",
+		default = true,
+		options = {
+			{ description = "Classic (No)", data = true },
+			{ description = "Standard (Yes)", data = false }
+		}
+	},
+	{
+		name = "SET_CLASSIC_INSULATION",
+		label = "Insulation Modifier",
+		hover = "Is more susceptible to overheating and freezing.",
+		default = true,
+		options = {
+			{ description = "Classic (No)", data = true },
+			{ description = "Standard (Yes)", data = false }
+		}
+	},
+	{
+		name = "SET_CLASSIC_GROGGINESS",
+		label = "Grogginess Modifier",
+		hover = "Takes longer to recover from grogginess.",
+		default = true,
+		options = {
+			{ description = "Classic (No)", data = true },
+			{ description = "Standard (Yes)", data = false }
+		}
+	},
+	{
+		name = "SET_CLASSIC_LIGHTNING",
+		label = "Lightning Modifier",
+		hover = "Has a higher chance of being struck by lightning.",
+		default = true,
+		options = {
+			{ description = "Classic (No)", data = true },
+			{ description = "Standard (Yes)", data = false }
 		}
 	}
 }
--- mod/modmain.lua
+++ mod/modmain.lua
@@ -1,4 +1,5 @@
 local TUNING = GLOBAL.TUNING
+local hasInstConfigs = (GetModConfigData("SET_CLASSIC_HUNGER_RATE") or GetModConfigData("SET_CLASSIC_HOUNDED_TARGET") or GetModConfigData("SET_CLASSIC_INSULATION"))
 
 if GetModConfigData("SET_CLASSIC_HEALTH") then
 	TUNING.WES_HEALTH = 113
@@ -12,12 +13,33 @@ if GetModConfigData("SET_CLASSIC_SANITY") then
 	TUNING.WES_SANITY = 150
 end
 
-if GetModConfigData("SET_CLASSIC_HUNGER_RATE") then
+if GetModConfigData("SET_CLASSIC_GROGGINESS") then
+	TUNING.WES_GROGGINESS_DECAY_RATE = TUNING.GROGGINESS_DECAY_RATE
+end
+
+if GetModConfigData("SET_CLASSIC_LIGHTNING") then
+	TUNING.WES_LIGHTNING_TARGET_CHANCE = TUNING.PLAYER_LIGHTNING_TARGET_CHANCE
+end
+
+if hasInstConfigs then 
 	AddPrefabPostInit("wes", function(inst)
 		if not GLOBAL.TheWorld.ismastersim then
 			return inst
 		end
-		
-		inst.components.hunger:SetRate(TUNING.WILSON_HUNGER_RATE * 1.25)
+
+		if GetModConfigData("SET_CLASSIC_HUNGER_RATE") then
+			inst.components.hunger:SetRate(TUNING.WILSON_HUNGER_RATE * 1.25)
+		end
+
+		if GetModConfigData("SET_CLASSIC_HOUNDED_TARGET") then
+			if inst.components.houndedtarget then
+				inst:RemoveComponent("houndedtarget")
+			end
+		end
+
+		if GetModConfigData("SET_CLASSIC_INSULATION") then
+			inst.components.temperature.inherentinsulation = TUNING.INSULATION_TINY
+			inst.components.temperature.inherentsummerinsulation = TUNING.INSULATION_TINY
+		end
 	end)
 end
